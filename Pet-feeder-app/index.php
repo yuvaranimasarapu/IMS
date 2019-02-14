2176cf84765ca49a6f9048157c50211271e3ea99
ljmesi <37740329+ljmesi@users.noreply.github.com>
2019-02-14 17:40:53 +0100

    Change index.php to share header and footer

--- Pet-feeder-app/index.php
+++ Pet-feeder-app/index.php
@@ -1,54 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-  <head>
-	<meta name="generator" content="Hugo 0.54.0" />
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta title="Pet Feeder App">
-
-
-
-
-<link rel="stylesheet" href="./public/sass/main.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-
-  <body>
-
-    <header class="site-header">
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="./index.html">Pet Feeder App</a>
-    </div>
-
-    
-    <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sign in <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a target="_blank" href="./pages/log-in/index.html" title="link to sign in page">Sign in</a></li>
-            <li><a href="./pages/sign-up/index.html" title="link to sign up page">Sign up</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a target="_blank" href="./index.html" title="link to log out page">Log out</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-</header>
+<?php include('./private/shared/header.php'); ?>
 
 <!-- TODO: Remove the styling from html here and change colour to the same blue as in the header. -->
 <h3 style="text-align: center"> Choose your Pet</h3>
@@ -106,30 +56,4 @@
       </div>
     </div>
 
-    <footer>
-    <div class="container">
-        <div class="row p20">
-            
-                <div class="col-md-4 text-center mt25">All rights reserved by <a target="_blank" href=".">Team Pet-feeder-app</a></div>
-            
-
-
-            <div class="col-md-4 text-center mt25">
-               
-               
-               
-               
-               
-            </div>
-
-        </div> 
-    </div>
-</footer>
-    
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-  <script src="./private/js/bootstrap.js"></script>
-  
-  </body>
-
-</html>
+<?php include('./private/shared/footer.php'); ?>
