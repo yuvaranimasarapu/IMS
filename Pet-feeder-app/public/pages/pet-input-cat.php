8aa80b1f82235fb464630e4b52f3cd64ce81fa7c
ljmesi <37740329+ljmesi@users.noreply.github.com>
2019-02-19 22:15:41 +0100

    Fix so that the main heading of the app is redirecting to back to main page; reduce the size of the Pet info input page's main heading

--- Pet-feeder-app/public/pages/pet-input-cat.php
+++ Pet-feeder-app/public/pages/pet-input-cat.php
@@ -31,7 +31,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="./index.php">Pet Feeder App</a>
+                    <a class="navbar-brand" href="../../index.php">Pet Feeder App</a>
                 </div>
 
 
